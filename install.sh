ca97c60705db96d760b7b22859e18ad6036a1819
yabuuuuu <yabuuuuu@gmail.com>
2014-05-15 00:17:14 +0900

    comment out .screenrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ ln -siv ~/dotfiles/.zshrc ~/.zshrc
 ln -siv ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -siv ~/dotfiles/.emacs ~/.emacs
 
-ln -siv ~/dotfiles/.screenrc ~/.screenrc
+#ln -siv ~/dotfiles/.screenrc ~/.screenrc
 
 #ln -siv ~/dotfiles/.bash_profile ~/.bash_profile
 #ln -siv ~/dotfiles/.bashrc ~/.bashrc
