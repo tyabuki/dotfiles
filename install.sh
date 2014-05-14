02dbb6e7f5dccb4d8b9f2a5d19ba96509211e881
yabuuuuu <yabuuuuu@gmail.com>
2014-05-14 23:45:26 +0900

    add files

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+ln -siv ~/dotfiles/.zshrc ~/.zshrc
+ln -siv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -siv ~/dotfiles/.emacs ~/.emacs
+
+ln -siv ~/dotfiles/.screenrc ~/.screenrc
+
+#ln -siv ~/dotfiles/.bash_profile ~/.bash_profile
+#ln -siv ~/dotfiles/.bashrc ~/.bashrc
