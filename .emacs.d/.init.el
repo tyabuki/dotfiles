ac1f309fea2a382e30ea33aa278fd08596755a08
yabuuuuu <yabuuuuu@gmail.com>
2014-05-18 19:38:13 +0900

    use solarized theme

--- .emacs.d/.init.el
+++ .emacs.d/.init.el
@@ -1,17 +1,73 @@
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;; ------------------------------------------------------------------------
+;; @ load-path
+
+;;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+;;; load-pathに追加するフォルダ
+; 2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
+(add-to-load-path "elisp")
+
 
 ;;; 表示関連
-(setq hl-line-face 'underline)
-(global-hl-line-mode) ; 行を目立たせる
+; Solarizedテーマの適用
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-emacs")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t) ; または(load-theme 'solarized-light t)
+
+;; フォントの指定（Solarizedテーマ適用時にcocoa版でコメントが豆腐化するのを防止）
+; ！！フォント指定は改めて調整中！！（これがあるとTerminal版で動かない）
+;(when (x-list-fonts "Ricty")
+;  (let* ((size 14)
+;         (asciifont "Ricty")
+;         (jpfont "Ricty")
+;         (h (* size 10))
+;         (fontspec)
+;         (jp-fontspec))
+;    (set-face-attribute 'default nil :family asciifont :height h)
+;    (setq fontspec (font-spec :family asciifont))
+;    (setq jp-fontspec (font-spec :family jpfont))
+;    (set-fontset-font nil 'japanese-jisx0208 jp-fontspec)
+;    (set-fontset-font nil 'japanese-jisx0212 jp-fontspec)
+;    (set-fontset-font nil 'japanese-jisx0213-1 jp-fontspec)
+;    (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+;    (set-fontset-font nil '(#x0080 . #x024F) fontspec)
+;    (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
+;)
+
+(setq-default line-spacing 0) ; 行間
+
+(global-hl-line-mode) ; 現在の行を目立たせる
+(setq hl-line-face 'underline) ; 目立たせるスタイル
+
 (global-highlight-changes-mode t) ; 変更箇所を目立たせる
 
-(show-paren-mode 1) ; 対応する括弧をハイライト
+(show-paren-mode t) ; 対応する括弧をハイライト
+(setq show-paren-delay 0) ; 強調表示のディレイ
+(setq show-paren-style 'expression) ; 括弧内全体を強調
+;(set-face-background 'show-paren-match-face "#500") ; 括弧内の色
 
 ; コメントの色
-(set-face-foreground 'font-lock-comment-face "pink")
+;(set-face-foreground 'font-lock-comment-face "pink")
 (set-face-foreground 'font-lock-comment-delimiter-face "pink")
 
 (column-number-mode t) ; 何文字目か表示
 (line-number-mode t) ; 何行目か表示
+(global-linum-mode t) ; 左側に評判号を表示
+
+; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name))
+)
+
+(setq-default show-trailing-whitespace t) ; 行末の空白を強調表示
+;(set-face-background 'trailing-whitespace "#b14770") ; 強調表示の色
 
 
 ;;; コード関連
@@ -22,7 +78,11 @@
 
 
 ;;; emacsの挙動関連
+(setq initial-scratch-message "") ; scratchの初期メッセージ消去
 (which-function-mode 1) ; 関数名を表示
 (setq visible-bell t) ; beepの代わりにwindowをブリンクさせる
 (setq vc-follow-symlinks t) ; 自動でsymlinkを辿る
 (setq mouse-drag-copy-region t) ; マウス選択で直ちにコピー．23まではdefault，24からnil．
+(fset 'yes-or-no-p 'y-or-n-p) ; yes or noをy or n
+
+
--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 
+## zsh
 ln -siv ~/dotfiles/.zshrc ~/.zshrc
-ln -siv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -siv ~/dotfiles/.emacs ~/.emacs
 
-#ln -siv ~/dotfiles/.screenrc ~/.screenrc
-
+## bash
 #ln -siv ~/dotfiles/.bash_profile ~/.bash_profile
 #ln -siv ~/dotfiles/.bashrc ~/.bashrc
+
+## tmux
+ln -siv ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+## emacs
+if [ -f ~/.emacs ]; then
+    echo "exists .emacs  rename .emacs.old"
+    mv ~/.emacs ~/.emacs.old
+fi
+ln -siv ~/dotfiles/.emacs ~/.emacs
+
+if [ -d ~/.emacs.d ]; then
+    echo "exists .emacs.d  rename .emacs.d.old"
+    mv ~/.emacs.d ~/.emacs.d.old
+fi
+ln -siv ~/dotfiles/.emacs.d ~/.emacs.d
+
+## screen
+#ln -siv ~/dotfiles/.screenrc ~/.screenrc
+
+
